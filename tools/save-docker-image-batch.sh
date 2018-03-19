733d2a998013c2b459dd56428f2a3695bfceb6c1
Juri Boxberger <juri.boxberger@gmx.net>
2018-03-19 21:47:28 +0100

    save docker image 10.5.5

--- tools/save-docker-image-batch.sh
+++ tools/save-docker-image-batch.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-./save-docker-image.sh --image\="gitlab/gitlab-ce:10.2.5-ce.0"
-./save-docker-image.sh --image\="postgres:10.1"
-#./save-docker-image.sh --image\="sameersbn/gitlab:10.2.5"
+./save-docker-image.sh --image\="sameersbn/gitlab:10.5.5"
 #./save-docker-image.sh --image\="sameersbn/postgresql:9.6-2"
-./save-docker-image.sh --image\="redis:3.2.11"
+#./save-docker-image.sh --image\="redis:3.2.11"
