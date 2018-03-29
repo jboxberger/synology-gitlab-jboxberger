a0f419b6df2c1c656a52a2778455ec10c4aac48d
Juri Boxberger <juri.boxberger@gmx.net>
2018-03-29 22:15:57 +0200

    GitLab update 10.6.0

--- tools/save-docker-image-batch.sh
+++ tools/save-docker-image-batch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./save-docker-image.sh --image\="sameersbn/gitlab:10.5.5"
+./save-docker-image.sh --image\="sameersbn/gitlab:10.6.0"
 #./save-docker-image.sh --image\="sameersbn/postgresql:9.6-2"
 #./save-docker-image.sh --image\="redis:3.2.11"
